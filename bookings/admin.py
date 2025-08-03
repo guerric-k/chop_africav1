8ded30b0bbd6eb8ef29ecce6d6426dbc0d0858a7
guerric-k <160793947+guerric-k@users.noreply.github.com>
2025-08-03 16:07:12 +0200

    add Review and MenuItem models

--- bookings/admin.py
+++ bookings/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import Table, Booking
+from .models import Table, Booking, MenuItem, Review
 
 # Register your models here.
 admin.site.register(Table)
 admin.site.register(Booking)
+admin.site.register(MenuItem)
+admin.site.register(Review)
--- bookings/models.py
+++ bookings/models.py
@@ -1,5 +1,20 @@
 from django.db import models
 from django.core.exceptions import ValidationError
+from django.core.validators import MinValueValidator, MaxValueValidator
+from django.contrib.auth.models import User
+
+class Review(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    rating = models.PositiveIntegerField(
+        default=5,
+        validators=[MinValueValidator(1), MaxValueValidator(5)]
+    )
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Review by {self.user}"
+
 from django.contrib.auth.models import User
 
 # Create your models here.
@@ -41,5 +56,25 @@ class Booking(models.Model):
             raise ValidationError("Sorry! There are no available slots for this day. please check another date.")
 
     def __str__(self):
-        return f"{self.user} - {self.date} @ {self.time} status {self.status}"
+        return f"Booking for {self.user} - {self.date} at {self.time} |status: {self.get_status_display()}"
 
+class MenuItem(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    is_available = models.BooleanField(default=True)
+    image = models.ImageField(upload_to='menu_images/', blank=True, null=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+class Review(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    rating = models.PositiveIntegerField(default=5, validators=[MinValueValidator(1), MaxValueValidator(5)]
+    )
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Review by {self.user}"
