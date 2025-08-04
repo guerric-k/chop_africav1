fcce12e57ae3ae9312cd50b3d27d63923f00494b
guerric-k <160793947+guerric-k@users.noreply.github.com>
2025-08-04 13:42:39 +0100

    add images, home page and configure urls

--- bookings/urls.py
+++ bookings/urls.py
@@ -1,6 +1,8 @@
 from . import views
 from django.urls import path
+from .views import HomePageView, MenuItemListView
 
 urlpatterns = [
-    path('', views.MenuItemListView.as_view(), name='menu'),
+    path('', HomePageView.as_view(), name='home'),              # Root/home page
+    path('menu/', MenuItemListView.as_view(), name='menu'),     # Menu page
 ]
--- bookings/views.py
+++ bookings/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.views.generic import ListView
 from .models import MenuItem
+from django.views.generic import TemplateView
+
 
 # Create your views here.
 
@@ -9,3 +11,6 @@ class MenuItemListView(ListView):
     template_name = 'menu/menu_list.html'  
     context_object_name = 'menu_items'     
     paginate_by = 6
+
+class HomePageView(TemplateView):
+    template_name = 'home.html'
